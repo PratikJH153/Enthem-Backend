593f218781c2242bae915bcf6e550a67251a335d
Yashkumar Vispute <yashvispute544@gmail.com>
2023-03-09 15:33:46 +0530

    pagination

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -121,13 +121,20 @@ const isUsernameExists = async (req: Request, res: Response, next: NextFunction)
 
 const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const page = +req.query.page || 1;
+    const pageSize = +req.query.pageSize || 10;
+    const skip = (page - 1) * pageSize;
+  
     const query = `
       MATCH (n:User)
       RETURN n.id AS id, n.username AS username, n.email AS email, n.age AS age,
         n.gender AS gender, n.photoURL AS photoURL,
-        n.latitude AS latitude, n.longitude AS longitude;
+        n.latitude AS latitude, n.longitude AS longitude
+      SKIP ${skip} LIMIT ${pageSize};
     `;
+    
     const result = await session.run(query);
+  
     if (result.records.length > 0) {
       const resultList = result.records.map(record => ({
         id: record.get('id'),
@@ -139,7 +146,7 @@ const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
         latitude: record.get('latitude'),
         longitude: record.get('longitude')
       }));
-      return res.status(200).json({ status: 200, resultList});
+      return res.status(200).json({ status: 200, resultList });
     } else {
       return res.status(404).json({ status: 404, data: "Sorry, No User yet on Enthem" });
     }
@@ -147,6 +154,7 @@ const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
     debugError(e.toString());
     return next(e);
   }
+  
 };
 
 
@@ -209,8 +217,6 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(e);
   }
 };
-
-
 
 
 
@@ -334,20 +340,23 @@ const compatibleUsers = async (req: Request, res: Response, next: NextFunction) 
     const skip: number = offset * max;
 
     const query = `
-
-      MATCH (u:User)-[:HAS_INTEREST]->(s:Activity)<-[:HAS_INTEREST]-(u2:User)
+      MATCH (u:User)-[:HAS_INTEREST]->(s:Activity)
       WHERE u.id = "${req.body.id}"
-      AND u2.id <> u.id 
+      WITH u, COLLECT(s) AS interests
+      MATCH (u2:User)-[:HAS_INTEREST]->(s2:Activity)
+      WHERE u2.id <> u.id
+      WITH u, u2, interests, COLLECT(s2) AS interests2
       WITH u, u2, 
-          [(u)-[:HAS_INTEREST]->(i:Activity) | i.username] AS u_interests,
-          [(u2)-[:HAS_INTEREST]->(i:Activity) | i.username] AS u2_interests
-      WITH u, u2, 
-          apoc.coll.intersection(u_interests, u2_interests) AS common_interests,
-          toFloat(size(apoc.coll.intersection(u_interests, u2_interests))) / toFloat(size(u_interests)) AS u_similarity,
-          toFloat(size(apoc.coll.intersection(u_interests, u2_interests))) / toFloat(size(u2_interests)) AS u2_similarity
-      RETURN DISTINCT u2.username as username, u2.email as email, u2.age as age, u2.gender as gender, u2.latitude as latitude, 
-                      u2.longitude as longitude, u2.photoURL as photoURL, 
-                      toInteger(((u_similarity + u2_similarity) / 2) * 100) AS match_percentage
+          REDUCE(s = [], x IN interests | 
+                  s + CASE WHEN x IN interests2 THEN x ELSE [] END) AS common_interests,
+          toFloat(size(REDUCE(s = [], x IN interests | s + x))) AS u_interests,
+          toFloat(size(interests2)) AS u2_interests
+      WITH u, u2, common_interests,
+          toFloat(size(common_interests)) / u_interests AS u_similarity,
+          toFloat(size(common_interests)) / u2_interests AS u2_similarity
+      RETURN u2.username AS username, u2.email AS email, u2.age AS age, u2.gender AS gender, u2.latitude AS latitude, 
+            u2.longitude AS longitude, u2.photoURL AS photoURL, 
+            toInteger(((u_similarity + u2_similarity) / 2) * 100) AS match_percentage
       ORDER BY match_percentage DESC
       SKIP ${skip} LIMIT ${max}
     
@@ -433,10 +442,9 @@ const interestsUser = async (req: Request, res: Response, next: NextFunction) =>
     `;
     const result2 = await session.run(query2);
     if (result2.records.length > 0) {
-      const interests = result2.records.map(record => record.get('interest'));
-      return res.status(200).json({ status: 200, interests });
+      return res.status(200).json({ status: 200, data: true });
     } else {
-      return res.status(409).json({ status: 409, message: "Please select interests for better engagement!" });
+      return res.status(404).json({ status: 404, data: false });
     }
   } catch (e) {
     debugError(e.toString());
