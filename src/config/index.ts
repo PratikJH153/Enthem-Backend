1c08a6e0ee00a2eadb75eed28619a49e3348074e
Pratik JH <56499739+PratikJH153@users.noreply.github.com>
2023-04-14 23:12:51 +0530

    config file changes made

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,9 +4,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 if(process.env.NODE_ENV === 'dev'){
   const envFound = dotenv.config();
-  if (envFound.error) {
-    throw new Error("⚠️  Couldn't find .env file  ⚠️");
-  }
 } 
 
 export default {
