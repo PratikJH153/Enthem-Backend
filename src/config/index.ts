b07b7590315c4db7def4211d4ed35c94d109ce9c
Pratik Jadhav <56499739+PratikJH153@users.noreply.github.com>
2024-07-24 23:44:41 -0500

    Working condition

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,9 +5,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if(process.env.NODE_ENV === 'dev'){
   const envFound = dotenv.config();
   if (envFound.error) {
-    // This error should crash whole process
-
-    throw new Error("⚠️  Couldn't find .env file  ⚠️");
   }
 }
 
